f1ea5e74d121e6bfe3dc0932ad234d77ced40794
Charles R. Portwood II <charlesportwoodii@erianna.com>
2026-01-25 20:22:12 -0600

    chore: update android path

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    namespace = "com.alaydriem.bvc.plugin.audio_permissions"
+    namespace = "com.charlesportwoodii.tauri.plugin.audio_permissions"
     compileSdk = 36
 
     defaultConfig {
@@ -37,15 +37,15 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.0")
     implementation("com.google.android.material:material:1.7.0")
-    
+
     // Unit testing
     testImplementation("junit:junit:4.13.2")
-    
+
     // Instrumented testing
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.test:runner:1.5.2")
     androidTestImplementation("androidx.test:rules:1.5.0")
-    
+
     implementation(project(":tauri-android"))
 }
